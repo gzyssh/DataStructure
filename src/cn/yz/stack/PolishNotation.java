f7c251fdc58b5aa5d33bd71fb976d9a87c8340b7
zhongyi.guo <zhongyi.guo@ruiruan.com>
2020-01-15 12:42:46 +0800

    逆波兰表达式

--- src/cn/yz/stack/PolishNotation.java
+++ src/cn/yz/stack/PolishNotation.java
@@ -1,5 +1,9 @@
 package cn.yz.stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * <p>
  *  逆波兰表达式
@@ -9,4 +13,44 @@ package cn.yz.stack;
  * @create 2020/01/14
  */
 public class PolishNotation {
+    public static void main(String[] args) {
+        String str="30 4 + 5 * 6 - ";
+        List<String> list = getListString(str);
+        int calculator = calculator(list);
+        System.out.printf("表达式：%s=%d\n",str,calculator);
+
+    }
+    public static List<String> getListString(String str){
+        String[] split = str.split(" ");
+        List<String> list=new ArrayList<>();
+        for (String s : split) {
+            list.add(s);
+        }
+        return list;
+    }
+    public static int calculator(List<String> list){
+        Stack<String> stack=new Stack<>();
+        for (String s : list) {
+            if(s.matches("\\d+")){
+                stack.push(s);
+            }else{
+                int num2=Integer.parseInt(stack.pop());
+                int num1=Integer.parseInt(stack.pop());
+                int res=0;
+                if(s.equals("+")){
+                    res=num1+num2;
+                }else if(s.equals("-")){
+                    res=num1-num2;
+                }else if(s.equals("*")){
+                    res=num1*num2;
+                }else if(s.equals("/")){
+                    res=num1/num2;
+                }else{
+                    throw new RuntimeException("运算符有误");
+                }
+                stack.push(String.valueOf(res));
+            }
+        }
+        return Integer.parseInt(stack.pop());
+    }
 }
