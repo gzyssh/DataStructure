7bfca3d895b423f7dbaa1f25839dfd8c9d95cf9d
zhongyi.guo <zhongyi.guo@ruiruan.com>
2020-01-16 13:54:56 +0800

    中缀转后缀表达式

--- src/cn/yz/stack/PolishNotation.java
+++ src/cn/yz/stack/PolishNotation.java
@@ -14,12 +14,70 @@ import java.util.Stack;
  */
 public class PolishNotation {
     public static void main(String[] args) {
-        String str="30 4 + 5 * 6 - ";
+       /* String str="30 4 + 5 * 6 - ";
         List<String> list = getListString(str);
-        int calculator = calculator(list);
-        System.out.printf("表达式：%s=%d\n",str,calculator);
+        int calculator = calculator(list);*/
+        String str="1+((2+3)*4)-5";
+        List<String> list = toIndexList(str);
+        List<String> listIndex = toListIndex(list);
+        System.out.println("中缀表达式为："+list);
+        System.out.println("后缀表达式为："+listIndex);
 
     }
+    public static List<String> toListIndex(List<String> list){
+        Stack<String> stack=new Stack();
+        List<String> arrayList=new ArrayList<>();
+        for (String s : list) {
+            if(s.matches("\\d+")){
+                arrayList.add(s);
+            }else if(s.equals("(")){
+                stack.push(s);
+            }else if(s.equals(")")){
+                while (!stack.peek().equals("(")){
+                    arrayList.add(stack.pop());
+                }
+                stack.pop();
+            }else{
+                while (stack.size()!=0&&(priority(stack.peek())>=priority(s))){
+                    arrayList.add(stack.pop());
+                }
+                stack.push(s);
+            }
+        }
+        while (stack.size()!=0){
+            arrayList.add(stack.pop());
+        }
+        return arrayList;
+    }
+    public static int priority(String oper){
+        if(oper.equals("*")||oper.equals("/")){
+            return 2;
+        }else if(oper.equals("+")||oper.equals("-")){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+    public static List<String> toIndexList(String str){
+        List<String> list=new ArrayList<>();
+        int index=0;
+        String s="";
+        do {
+            if(str.charAt(index)<48||str.charAt(index)>57){
+                list.add(String.valueOf(str.charAt(index)));
+                index++;
+            }else{
+                s="";
+                while (index<str.length()&&(str.charAt(index)>=48&&str.charAt(index)<=57)){
+                    s+=str.charAt(index);
+                    index++;
+                }
+                list.add(s);
+            }
+        }while (index<str.length());
+        return list;
+    }
+
     public static List<String> getListString(String str){
         String[] split = str.split(" ");
         List<String> list=new ArrayList<>();
