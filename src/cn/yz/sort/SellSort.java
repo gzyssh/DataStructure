3c91ecb50b8e64bfb6ea6cf99b2fc825056480ed
zhongyi.guo <zhongyi.guo@ruiruan.com>
2020-01-17 16:05:00 +0800

    希尔排序-交换式+移位方式

--- src/cn/yz/sort/SellSort.java
+++ src/cn/yz/sort/SellSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * <p>
- *  希尔排序-交换式
+ *  希尔排序
  * </p>
  *
  * @author guozy
@@ -13,9 +13,14 @@ import java.util.Arrays;
 public class SellSort {
     public static void main(String[] args) {
         int[] ary={8,9,1,7,2,3,5,4,6,0};
-        sellSort(ary);
+        sellSortTwo(ary);
         System.out.println(Arrays.toString(ary));
     }
+
+    /**
+     * 交换式
+     * @param ary
+     */
     public static void sellSort(int[] ary){
         int temp;
         for (int gap=ary.length/2;gap>0;gap/=2){
@@ -26,6 +31,27 @@ public class SellSort {
                         ary[j]=ary[j+gap];
                         ary[j+gap]=temp;
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * 移位式
+     * @param ary
+     */
+    public static void sellSortTwo(int[] ary){
+        int temp,j;
+        for (int gap=ary.length/2;gap>0;gap/=2){
+            for (int i = gap; i < ary.length; i++) {
+                temp=ary[i];
+                j=i;
+                while (j-gap>=0&&temp<ary[j-gap]){
+                    ary[j]=ary[j-gap];
+                    j-=gap;
+                }
+                if(!(j==i)){
+                    ary[j]=temp;
                 }
             }
         }
